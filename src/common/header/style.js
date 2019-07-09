71c28eb85cd44a5891f00ef009ac2b46da67f796
tho <290215052@qq.com>
2019-07-09 14:51:42 +0800

    first commit

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -2,11 +2,7 @@ import styled from 'styled-components';
 import logoPic from '../../statics/logo.png';
 import navbetaPic from '../../statics/navbeta.png';
 
-// export const FixedHeader = styled.div`
-// position: fixed;
-// height:56px;
-// border-bottom: 1px solid #f0f0f0;
-// `
+
 
 export const HeaderWrapper = styled.div`
     z-index:1;
